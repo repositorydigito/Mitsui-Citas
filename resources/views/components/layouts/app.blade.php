73e293714dd8de5420a9220991fe7ce55bb21531
ricardo-digito <rrivera@digito.pe>
2026-01-09 13:56:01 -0500

    feat: img

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,9 @@
         <meta name="application-name" content="{{ config('app.name') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="manifest" href="{{ asset('manifest.json') }}">
+        <link rel="apple-touch-icon" href="{{ asset('images/LOGOTEMA1.png') }}">
+        <meta name="theme-color" content="#ffffff">
 
         <title>{{ $title ?? config('app.name') }}</title>
 
@@ -42,10 +45,6 @@
         <script src="https://cdn.tailwindcss.com"></script>
         @livewireStyles
 
-        <link rel="manifest" href="{{ asset('manifest.json') }}">
-        <link rel="apple-touch-icon" href="{{ asset('images/LOGOTEMA1.png') }}">
-        <meta name="theme-color" content="#ffffff">
-        
     </head>
     
     <body class="antialiased">
